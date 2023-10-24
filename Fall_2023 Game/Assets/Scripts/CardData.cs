e409e798ce0dc1cc5e4a336730e881022594d7ce
Cory <Cannedcornflakes@gmail.com>
2023-10-24 08:16:58 -0600

    Added more to CardData

--- Fall_2023 Game/Assets/Scripts/CardData.cs
+++ Fall_2023 Game/Assets/Scripts/CardData.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 public class CardData : ScriptableObject
 {
     public new string name;
+    public string subName;
     public string description;
     
     public Sprite artwork;
-
+    public Sprite cardType;
+    
     public int cost;
     public int damage;
     public int health;
+    public int spellSlots;
 
+    
     public void Print()
     {
         Debug.Log(name + " " + description + ", The card costs: " + cost);
--- Fall_2023 Game/Assets/Scripts/CardDisplay.cs
+++ Fall_2023 Game/Assets/Scripts/CardDisplay.cs
@@ -10,16 +10,20 @@ public class CardDisplay : MonoBehaviour
     public CardData card;
 
     public Text nameText;
+    public Text subNameText;
     public Text descriptionText;
     public Image artworkImage;
+    public Image cardImage;
 
     public Text costText;
     
     void Start()
     {
         nameText.text = card.name;
+        subNameText.text = card.subName;
         descriptionText.text = card.description;
-
+        
         artworkImage.sprite = card.artwork;
+        cardImage.sprite = card.cardType;
     }
 }
